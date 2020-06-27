09bdd1bd9caed72b4c5d1d176f94ce6b44d9abbf
Sergio <ortiz.sergio3814@gmail.com>
2020-06-26 21:30:37 -0400

    feat: hide one time service prices when on the subscriptions tab

--- js/tab-switch.js
+++ js/tab-switch.js
@@ -1,15 +1,18 @@
 const oneTimeServiceTab = document.getElementById("one-time-service-tab");
 const subscriptionsTab = document.getElementById("subscriptions-tab");
+const oneTimeServiceTable = document.getElementById("one-time-service-table");
 
 function switchToOneTimeServiceTab() {
   oneTimeServiceTab.style.background = "#fff";
   subscriptionsTab.style.background = "#f4f4f4";
+  oneTimeServiceTable.style.display = "table";
   return;
 }
 
 function switchToSubscriptionsTab() {
   subscriptionsTab.style.background = "#fff";
   oneTimeServiceTab.style.background = "#f4f4f4";
+  oneTimeServiceTable.style.display = "none";
   return;
 }
 
