85641d98b873122f7ac15c49251bd8175fd0449a
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2021-01-05 20:33:22 -0500

    refactor(carousel): decouple javascript

--- js/carousel.js
+++ js/carousel.js
@@ -17,38 +17,17 @@ function updateIndexValue(newIndex) {
   currentIndex = newIndex;
 }
 
-const imageElement = document.getElementById("image-element");
-
-const imageSourceArray = [
-  "./assets/1.png",
-  "./assets/2.png",
-  "./assets/3.png",
-  "./assets/4.png",
-  "./assets/5.png",
-  "./assets/6.png",
-  "./assets/7.png",
-  "./assets/8.png",
-  "./assets/9.png",
-  "./assets/10.png",
-  "./assets/11.png",
-  "./assets/12.png",
-  "./assets/13.png",
-  "./assets/14.png",
-  "./assets/15.png",
-  "./assets/16.png",
-];
-
 function setImageSourceFromSourceArray(image, arr, index) {
   image.src = arr[index];
 }
 
-function updateImage() {
-  defineNextIndexValue(getNextIndexInArray, imageSourceArray, currentIndex);
+function updateImage(image, arr) {
+  defineNextIndexValue(getNextIndexInArray, arr, currentIndex);
   updateIndexValue(nextIndex);
-  setImageSourceFromSourceArray(imageElement, imageSourceArray, currentIndex);
+  setImageSourceFromSourceArray(image, arr, currentIndex);
 }
 
-export default (function main() {
-  updateImage();
-  window.setInterval(updateImage, 2000);
-})();
+export function main(image, arr) {
+  updateImage(image, arr);
+  window.setInterval(updateImage, 2000, image, arr);
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 import "./toggle-menu.js";
 import "./tab-switch.js";
 import "./clear-message.js";
-import "./carousel.js";
+import { main as carousel } from "./carousel.js";
+
+const imageElement = document.getElementById("image-element");
+const imageSourceArray = [
+  "./assets/1.png",
+  "./assets/2.png",
+  "./assets/3.png",
+  "./assets/4.png",
+  "./assets/5.png",
+  "./assets/6.png",
+  "./assets/7.png",
+  "./assets/8.png",
+  "./assets/9.png",
+  "./assets/10.png",
+  "./assets/11.png",
+  "./assets/12.png",
+  "./assets/13.png",
+  "./assets/14.png",
+  "./assets/15.png",
+  "./assets/16.png",
+];
+
+carousel(imageElement, imageSourceArray);
