28b4f764ef73b67c98c49820fc47619563bb8293
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2021-01-08 22:00:39 -0500

    feat(refactor): refactor carousel code based on Alvaro's suggestions

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,6 @@
-let currentIndex = -1;
-let nextIndex;
+let indexState = -1;
 
-function getNextIndexInArray(arr, index) {
+function nextIndex(arr, index) {
   if (index === arr.length - 1) {
     return 0;
   } else {
@@ -9,25 +8,12 @@ function getNextIndexInArray(arr, index) {
   }
 }
 
-function defineNextIndexValue(getter, arr, index) {
-  nextIndex = getter(arr, index);
+function updateImage(imageElement, imagePaths) {
+  indexState = nextIndex(imagePaths, indexState);
+  imageElement.src = imagePaths[indexState];
 }
 
-function updateIndexValue(newIndex) {
-  currentIndex = newIndex;
-}
-
-function setImageSourceFromSourceArray(image, arr, index) {
-  image.src = arr[index];
-}
-
-function updateImage(image, arr) {
-  defineNextIndexValue(getNextIndexInArray, arr, currentIndex);
-  updateIndexValue(nextIndex);
-  setImageSourceFromSourceArray(image, arr, currentIndex);
-}
-
-export function carousel(image, arr) {
-  updateImage(image, arr);
-  window.setInterval(updateImage, 2000, image, arr);
+export function carousel(imageElement, imagePaths) {
+  updateImage(imageElement, imagePaths);
+  window.setInterval(updateImage, 2000, imageElement, imagePaths);
 }
